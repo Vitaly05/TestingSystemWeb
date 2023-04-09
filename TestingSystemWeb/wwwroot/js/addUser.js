4a14006c36d122cabb3dbcdcca64e8b9912d76c7
Vitaly05 <vitalik050405@gmail.com>
2023-04-09 13:28:05 +0300

    Added fields validation when add new user

--- TestingSystemWeb/wwwroot/js/addUser.js
+++ TestingSystemWeb/wwwroot/js/addUser.js
@@ -1,27 +1,29 @@
 document.querySelector("#addUserButton").addEventListener("click", async e => {
     e.preventDefault()
 
-
-    const formData = new FormData(addUserForm)
-    const userData = Object.fromEntries(formData.entries());
-
     let role
     document.querySelectorAll("input[name='role']").forEach(radioButton => {
         if (radioButton.checked) {
             role = radioButton.value;
         }
     })
-    userData.role = role
 
-    await fetch("users/add", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(userData)
-    }).then(response => {
-        if (response.ok === true) {
-            window.location.href = "/"
-        }
-    })
+    if (fieldsAreValid(role)) {
+        const formData = new FormData(addUserForm)
+        const userData = Object.fromEntries(formData.entries());
+    
+        userData.role = role
+    
+        await fetch("users/add", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(userData)
+        }).then(response => {
+            if (response.ok === true) {
+                window.location.href = "/"
+            }
+        })
+    }
 })
 
 document.querySelectorAll(".js-input").forEach(jsInput => {
@@ -29,3 +31,33 @@ document.querySelectorAll(".js-input").forEach(jsInput => {
         e.preventDefault()
     })
 })
+
+
+function fieldsAreValid(role) {
+    let allFieldsAreValid = true
+
+    Array.from(document.querySelectorAll("input[name]")).reverse().forEach(questionField => {
+        if (role !== "Student" && questionField.name === "group") {
+            undefined
+        } else {
+            fieldIsValid(questionField) ? undefined : allFieldsAreValid = false
+        }
+    })
+
+    return allFieldsAreValid
+}
+
+function fieldIsValid(field) {
+    if (field.value === null || field.value.trim() === "") {
+        field.parentNode.classList.add("invalid-field")
+        field.scrollIntoView({
+            behavior: "smooth",
+            block: "center"
+        })
+        
+        return false
+    } else {
+        field.parentNode.classList.remove("invalid-field")
+        return true
+    }
+}
